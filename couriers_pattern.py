c3a186e5aff3a5e552cf3a3a48dba3829d5e084f
Samah Ali <samahali9226@gmail.com>
2022-01-29 19:56:14 +0200

    Add files via upload

---
+++ couriers_pattern.py
@@ -0,0 +1,68 @@
+from abc import abstractmethod, ABC
+from decimal import Decimal
+
+
+class Shipment(ABC):
+    def __init__(self):
+        self.cancel_shipping_behavior = None
+
+    @abstractmethod
+    def create_shipment(self, reference_number: str, merchant: object, customer: object, weight: dict):
+        pass
+
+    @abstractmethod
+    def track_shipment_status(self, reference_number: str):
+        pass
+
+    @abstractmethod
+    def map_shipment_status(self):
+        pass
+
+    def perform_cancel_shipping(self, reference_number: str):
+        self.cancel_shipping_behavior.cancel(reference_number)
+
+
+class WayBill(ABC):
+    @abstractmethod
+    def create_waybill(self):
+        pass
+
+    @abstractmethod
+    def print_waybill(self):
+        pass
+
+
+class Courier(WayBill, Shipment):
+    def create_shipment(self, reference_number: str, merchant: object, customer: object, weight: Decimal):
+        pass
+
+    def track_shipment_status(self, reference_number: str):
+        pass
+
+    def map_shipment_status(self):
+        pass
+
+    def create_waybill(self):
+        pass
+
+    def print_waybill(self):
+        pass
+
+
+class CancelShippingBehavior(ABC):
+
+    @abstractmethod
+    def cancel(self):
+        pass
+
+
+class CancelShipping(CancelShippingBehavior):
+    def cancel(self, reference_number: str):
+        pass
+
+
+class CourierWithCanceling(Courier):
+
+    def __init__(self):
+        super().__init__()
+        self.cancel_shipping_behavior = CancelShipping()
